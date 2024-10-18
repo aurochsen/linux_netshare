a15463d8161629e280e877980ea4d8ade44f7c7c
Jason <34282002+aurochsen@users.noreply.github.com>
2024-10-17 21:02:27 -0400

    Create linux_netshare.sh

---
+++ linux_netshare.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+# name of the ethernet gadget interface on the host
+USB_IFACE=${1:-enp0s20f0u1}
+USB_IFACE_IP=10.0.0.1
+USB_IFACE_NET=10.0.0.0/24
+# host interface to use for upstream connection
+UPSTREAM_IFACE=${2:-enxe4b97aa99867}
+
+# Check if IP is already assigned to the interface
+if ! ip addr show "$USB_IFACE" | grep -q "$USB_IFACE_IP"; then
+    ip addr add "$USB_IFACE_IP/24" dev "$USB_IFACE"
+fi
+
+ip link set "$USB_IFACE" up
+
+iptables -A FORWARD -o "$UPSTREAM_IFACE" -i "$USB_IFACE" -s "$USB_IFACE_NET" -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -t nat -F POSTROUTING
+iptables -t nat -A POSTROUTING -o "$UPSTREAM_IFACE" -j MASQUERADE
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
